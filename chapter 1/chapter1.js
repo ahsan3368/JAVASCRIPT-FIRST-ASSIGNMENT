4de8bf6ee96e879661fcddc212ccd5b1a3b1f423
Ahsan <ahsantariq1401@gmail.com>
2024-02-28 20:38:50 +0500

    Add files via upload

---
+++ chapter 1/chapter1.js
@@ -0,0 +1,16 @@
+
+
+// question 1
+alert("Hello world!");
+
+// question 2
+ alert("Welocome to JS land \n Happy Coding")
+ 
+//  question3
+alert("Welcome to Js Land")
+
+// question4
+alert("Happy Coding")
+
+// question5
+alert("Hello I can run Js THrough my console!")
