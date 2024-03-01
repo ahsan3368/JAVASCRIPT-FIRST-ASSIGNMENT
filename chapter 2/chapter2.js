844f12e1f5155ed165be664a0ffb61b0bbe3c7df
Ahsan <ahsantariq1401@gmail.com>
2024-03-01 18:02:14 +0500

    Add files via upload

---
+++ chapter 2/chapter2.js
@@ -0,0 +1,36 @@
+// Chapter 2
+
+// Q1
+var username = "Ahsan"
+alert(username)
+
+// Q2
+var myname = "Ahsan tariq"
+alert("My name is " + myname)
+
+// Q3
+var message = "Hello World"
+alert(message)
+
+// Q4
+var m_name = "Ahsan Tariq"
+var age = 19
+var value = "Web Developer"
+alert("my name is: " + m_name)
+alert("I'm " + age)
+alert('I am learning ' + value)
+
+// Q5
+var pizza = " PIZZA \n PIZZ \n PIZ \n PI \n P"
+alert(pizza)
+
+// Q6
+var m_email = "My email address is "
+var email = "ahsantariq1401@gmail.com"
+alert(m_email + email)
+
+// Q7
+
+var book = "A smarter way to learn Javascript"
+alert("I am trying to learn from the book " + book)
+
