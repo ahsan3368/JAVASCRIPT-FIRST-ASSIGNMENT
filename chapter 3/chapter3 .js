785797e4c14a00a55d93c86ee2c4560320274b54
Ahsan <ahsantariq1401@gmail.com>
2024-03-01 18:19:39 +0500

    Add files via upload

---
+++ chapter 3/chapter3 .js
@@ -0,0 +1,20 @@
+// Chapter 2
+
+// Q1
+var age = 18
+alert("I am " + age + " years old")
+
+// Q2
+var visit = 14
+alert("You have visited this website " + visit + " times.")
+
+// Q3
+var birthYear = 2004
+document.write("My birth year is " + birthYear + ".<br>")
+document.write("Data type of my declared variable is Number. <br>")
+
+// Q4
+var visitorName = "Ahsan "
+var product = " Shirt"
+var quantity = 5
+document.write(visitorName + " ordered " + quantity + product + "(s) on XYZ Clothing Store.")
